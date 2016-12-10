f1679d705e79ec5f4f910c8815e462f1ee329b3e
mertyurdakul <mrtyrdkl4@gmail.com>
2016-12-10 20:11:34 +0300

    user variables,getters and setters are done bitches!

--- src/com/koala/app/client/data/user/User.java
+++ src/com/koala/app/client/data/user/User.java
@@ -7,8 +7,12 @@ package com.koala.app.client.data.user;
  */
 public class User {
 
-
     private String fullName;
+    private String password;
+    private String email;
+    private String username;
+    private String phoneNumber;
+
 
     public String getFullName() {
         return fullName;
@@ -16,5 +20,37 @@ public class User {
 
     public void setFullName(String fullName) {
         this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
