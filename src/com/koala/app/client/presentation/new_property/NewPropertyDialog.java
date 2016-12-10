b0b650ced6d9713ceb1162d00f0416151ccb1b23
mertyurdakul <mrtyrdkl4@gmail.com>
2016-12-10 19:14:12 +0300

    new property fxml has done bitches!

---
+++ src/com/koala/app/client/presentation/new_property/NewPropertyDialog.java
@@ -0,0 +1,12 @@
+package com.koala.app.client.presentation.new_property;
+
+import com.koala.app.client.presentation.CustomDialog;
+
+/**
+ * Created by mert on 10.12.2016.
+ */
+public class NewPropertyDialog extends CustomDialog {
+    public NewPropertyDialog() {
+        super("new_property.fxml");
+    }
+}
