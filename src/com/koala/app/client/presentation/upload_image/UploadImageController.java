823af307d5628af1a478e6aaf645ffd293386b84
mertyurdakul <mrtyrdkl4@gmail.com>
2016-12-10 19:43:11 +0300

    upload image fxml has done bitches!

---
+++ src/com/koala/app/client/presentation/upload_image/UploadImageController.java
@@ -0,0 +1,39 @@
+package com.koala.app.client.presentation.upload_image;
+
+/**
+ * Created by mert on 10.12.2016.
+ */
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class UploadImageController {
+
+    @FXML
+    private ImageView uploaded3;
+
+    @FXML
+    private ImageView uploaded2;
+
+    @FXML
+    private ImageView uploaded1;
+
+    @FXML
+    private ImageView uploaded6;
+
+    @FXML
+    private ImageView uploaded5;
+
+    @FXML
+    private ImageView uploaded4;
+
+    @FXML
+    void onAddNew(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onFinish(ActionEvent event) {
+
+    }
+}
