18b27dce31966d15fd35a8c2295c6c989d7513ed
Asus-pc <zlmyldz96@gmail.com>
2016-12-10 18:50:37 +0300

    Message Controller JavaFX

---
+++ src/com/koala/app/client/presentation/message/MessageController.java
@@ -0,0 +1,24 @@
+package com.koala.app.client.presentation.message;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+
+/**
+ * Created by ozlem on 10.12.2016.
+ */
+public class MessageController {
+
+    @FXML
+    private ListView<?> contactListView;
+
+    @FXML
+    private TextArea messages;
+
+    @FXML
+    void onClickSend(ActionEvent event) {
+
+    }
+
+}
+
