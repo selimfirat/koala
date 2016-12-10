bed7db6d4a9c38976af453e001c27d689ba5ec4e
Asus-pc <zlmyldz96@gmail.com>
2016-12-10 18:29:56 +0300

    Register Controller JavaFX

--- src/com/koala/app/client/presentation/register/RegisterController.java
+++ src/com/koala/app/client/presentation/register/RegisterController.java
@@ -1,15 +1,43 @@
 package com.koala.app.client.presentation.register;
 
-import com.koala.app.client.presentation.IController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-/**
- * Author: Selim Fırat Yılmaz - mrsfy
- * Version: 1.0.0
- * Creation Date: 17.11.2016.
- */
-public class RegisterController implements IController {
-    @Override
-    public void init() {
+public class RegisterController {
+
+    @FXML
+    private TextField passwordAgainTF;
+
+    @FXML
+    private TextField usernameTF;
+
+    @FXML
+    private TextField nameTF;
+
+    @FXML
+    private TextField emailTF;
+
+    @FXML
+    private TextField phoneNumberTF;
+
+    @FXML
+    private TextArea passwordTF;
+
+    @FXML
+    void onClickCreate(ActionEvent event) {
 
     }
+
+    @FXML
+    void onClickCancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void d72d2d(ActionEvent event) {
+
+    }
+
 }
