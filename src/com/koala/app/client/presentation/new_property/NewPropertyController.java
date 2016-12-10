5d61834ffa9f341dc0266fa78bbc50ab56e1dcc6
mertyurdakul <mrtyrdkl4@gmail.com>
2016-12-10 18:47:50 +0300

    new property gui has done bitches!

---
+++ src/com/koala/app/client/presentation/new_property/NewPropertyController.java
@@ -0,0 +1,51 @@
+package com.koala.app.client.presentation.new_property;
+
+/**
+ * Created by mert on 10.12.2016.
+ */
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class NewPropertyController {
+
+    @FXML
+    private TextField priceTF;
+
+    @FXML
+    private TextField roomsTF;
+
+    @FXML
+    private TextField ageTF;
+
+    @FXML
+    private TextField furnisedTF;
+
+    @FXML
+    private TextField bathroomsTF;
+
+    @FXML
+    private TextField titleTF;
+
+    @FXML
+    private TextField sizeTF;
+
+    @FXML
+    private TextField commentsTF;
+
+    @FXML
+    void onCancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onAddProperty(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onChooseImage(ActionEvent event) {
+
+    }
+
+}
