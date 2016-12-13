0208e6b391c088120333d2400e075e5a4f2ebd12
Asus-pc <zlmyldz96@gmail.com>
2016-12-13 15:40:36 +0300

    My own propertiesi 3 tane var gibi yaptım düzeltmeyi öğrenene kadar

--- src/com/koala/app/client/presentation/property/PropertyController.java
+++ src/com/koala/app/client/presentation/property/PropertyController.java
@@ -1,7 +1,80 @@
 package com.koala.app.client.presentation.property;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 /**
- * Created by Asus-pc on 12.12.2016.
+ * Created by Ozlem on 12.12.2016.
  */
+
 public class PropertyController {
+
+    @FXML
+    private ImageView image3;
+
+    @FXML
+    private Label priceLabel2;
+
+    @FXML
+    private Label priceLabel1;
+
+    @FXML
+    private Label idLabel3;
+
+    @FXML
+    private Label idLabel2;
+
+    @FXML
+    private Label idLabel1;
+
+    @FXML
+    private Label priceLabel3;
+
+    @FXML
+    private ImageView image1;
+
+    @FXML
+    private ImageView image2;
+
+    @FXML
+    private Label nameLabel3;
+
+    @FXML
+    private Label nameLabel2;
+
+    @FXML
+    private Label nameLabel1;
+
+    @FXML
+    void onClickDelete1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickEdit1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickDelete2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickEdit2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickDelete3(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickEdit3(ActionEvent event) {
+
+    }
+
 }
+
