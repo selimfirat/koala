df86716571ff263c8b7c1917c64bc59c1299ea79
mrsfy <mrsfy@outlook.com>
2016-12-13 22:37:39 +0300

    Added Location to House model

--- src/com/koala/app/client/data/house/House.java
+++ src/com/koala/app/client/data/house/House.java
@@ -10,13 +10,19 @@ import com.koala.app.client.data.user.User;
 public class House {
 
 
+    private final String id;
+    private User seller;
+    private House.Types houseType;
+    private Location location;
+    private HouseFeatures houseFeatures;
+
     public enum Types {
         FOR_RENT,
         FOR_SALE
+
     }
 
     private class HouseFeatures {
-
         private String comments;
         private boolean furnished;
         private int bathroomNumber;
@@ -26,94 +32,90 @@ public class House {
         private int ageOfBuilding;
         private int size;
         private int roomNumber;
+
         private String title;
-        
+
         public void setComments(String comments) {
             this.comments = comments;
         }
-        
+
         public String getComments() {
             return comments;
         }
-        
+
         public void setFurnishedInfo(boolean furnished) {
             this.furnished = furnished;
         }
-        
+
         public boolean getFurnishedInfo() {
             return furnished;
         }
-        
+
         public void setBathroomNumber(int bathroomNumber) {
             this.bathroomNumber = bathroomNumber;
         }
-        
+
         public int getBathroomNumber() {
             return bathroomNumber;
         }
-        
+
         public void setCurrentFloor(int currentFloor) {
             this.currentFloor = currentFloor;
         }
-        
+
         public int getCurrentFloor() {
             return currentFloor;
         }
-        
+
         public void setTotalFloor(int totalFloor) {
             this.totalFloor = totalFloor;
         }
-        
+
         public int getTotalFloor() {
             return totalFloor;
         }
-        
+
         public void setPrice(int price) {
             this.price = price;
         }
-        
+
         public int getPrice() {
             return price;
         }
-        
+
         public void setAge(int ageOfBuilding) {
             this.ageOfBuilding = ageOfBuilding;
         }
-        
+
         public int getAge() {
             return ageOfBuilding;
         }
-        
+
         public void setSize(int size) {
             this.size = size;
         }
-        
+
         public int getSize() {
             return size;
         }
-        
+
         public void setRoomNumber(int roomNumber) {
             this.roomNumber = roomNumber;
         }
-        
+
         public int getRoomNumber() {
             return roomNumber;
         }
-        
+
         public void setTitle(String title) {
             this.title = title;
         }
-        
         public String getTitle() {
             return title;
         }
+
+
     }
-
-
-    private final String id;
-    private User seller;
-    private House.Types houseType;
-    private HouseFeatures houseFeatures;
 
     public House(String id, User seller, Types houseType, HouseFeatures houseFeatures){
         this.id = id;
@@ -122,6 +124,16 @@ public class House {
         this.houseFeatures = houseFeatures;
 
     }
+
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
     public String getId() {
         return id;
     }
--- src/com/koala/app/client/domain/houses/AddHouseUseCase.java
+++ src/com/koala/app/client/domain/houses/AddHouseUseCase.java
@@ -1,7 +1,12 @@
 package com.koala.app.client.domain.houses;
 
+import com.koala.app.client.data.house.House;
+
 /**
  * Created by mrsfy on 13-Dec-16.
  */
 public class AddHouseUseCase extends SaveHouseUseCase {
+    public AddHouseUseCase(House house) {
+        super(house);
+    }
 }
