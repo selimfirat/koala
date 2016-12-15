b8c0cfccda5c38dcc9954958459237a1c59b7f1a
mertyurdakul <mrtyrdkl4@gmail.com>
2016-12-15 16:20:30 +0300

    new_property reviewed bitches!

--- src/com/koala/app/client/presentation/new_property/NewPropertyController.java
+++ src/com/koala/app/client/presentation/new_property/NewPropertyController.java
@@ -3,48 +3,55 @@ package com.koala.app.client.presentation.new_property;
 /**
  * Created by mert on 10.12.2016.
  */
-import com.koala.app.client.presentation.IController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 
-public class NewPropertyController implements IController{
+public class NewPropertyController {
+
+    @FXML
+    private TextField commentTF;
 
     @FXML
     private TextField priceTF;
 
     @FXML
-    private TextField roomsTF;
-
-    @FXML
     private TextField ageTF;
 
     @FXML
-    private TextField furnisedTF;
+    private CheckBox furnished_check_box;
+
+    @FXML
+    private TextField titleRF;
 
     @FXML
     private TextField bathroomsTF;
 
     @FXML
-    private TextField titleTF;
+    private ChoiceBox<?> rooms_choice_box;
 
     @FXML
     private TextField sizeTF;
 
     @FXML
-    private TextField commentsTF;
+    void onAddProperty(ActionEvent event) {
 
-    @FXML
-    void onCancel(ActionEvent event) {
-        Node node = (Node) event.getSource();
-        Stage stage = (Stage) node.getScene().getWindow();
-        stage.close();
     }
 
     @FXML
-    void onAddProperty(ActionEvent event) {
+    void onCancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ff000d(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCheckFurnished(ActionEvent event) {
 
     }
 
@@ -53,8 +60,4 @@ public class NewPropertyController implements IController{
 
     }
 
-    @Override
-    public void init() {
-
-    }
 }
