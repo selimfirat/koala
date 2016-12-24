6dabf4a7045512d7c5400d3a6618899dcd463016
denizevr <deniz.evrensel@ug.bilkent.edu.tr>
2016-12-24 17:09:14 +0300

    Update ServerConnectionException.java

--- src/com/koala/app/client/data/ServerConnectionException.java
+++ src/com/koala/app/client/data/ServerConnectionException.java
@@ -9,18 +9,29 @@ import com.koala.app.client.data.NetworkException;
  */
 public class ServerConnectionException extends NetworkException {
 
+    //default ServerConnectionException
     public ServerConnectionException () {
 
     }
 
+    /*ServerConnectionException
+     *@param message
+     */
     public ServerConnectionException (String message) {
         super (message);
     }
 
+    /*ServerConnectionException
+     *@param cause
+     */
     public ServerConnectionException (Throwable cause) {
         super (cause);
     }
 
+    /*ServerConnectionException
+     *@param message
+     *@param cause
+     */
     public ServerConnectionException (String message, Throwable cause) {
         super (message, cause);
     }
