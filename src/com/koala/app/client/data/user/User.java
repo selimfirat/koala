836e93af4cc1a0920bc6838de09340b88bcf5a6c
denizevr <deniz.evrensel@ug.bilkent.edu.tr>
2016-12-24 17:01:53 +0300

    Update User.java

--- src/com/koala/app/client/data/user/User.java
+++ src/com/koala/app/client/data/user/User.java
@@ -9,8 +9,8 @@ import org.jongo.marshall.jackson.oid.MongoId;
  */
 public class User {
 
-    @MongoId
     //variables for user object
+    @MongoId
     private String id;
     private String fullName;
     private String password;
@@ -18,7 +18,14 @@ public class User {
     private String username;
     private String phoneNumber;
 
-    //user object with parameters id, fullname, password, email, username, phoneNumber
+    /*User object with given parameters
+     *@param id
+     *@param fullName
+     *@param password
+     *@param email
+     *@param username
+     *@param phoneNumber
+     */
     public User(String id, String fullName, String password, String email, String username, String phoneNumber) {
         this.id = id;
         this.fullName = fullName;
@@ -33,62 +40,86 @@ public class User {
 
     }
 
-    //getFullName method
+    /*getFullName method to get the fullname of the user
+     *returns fullName
+     */
     public String getFullName() {
         return fullName;
     }
 
-    //setFullName method
+    /*setFullName method to set the fullname of the user
+     *@param fullName
+     */
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
 
-    //getPassword method
+    /*getPassword method to get the password of the user
+     *returns password
+     */
     public String getPassword() {
         return password;
     }
 
-    //setPassword method
+    /*setPassword method to set the password of the user
+     *@param password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
-    //getEmail method
+    /*getEmail method to get the email of the user
+     *returns email
+     */
     public String getEmail() {
         return email;
     }
 
-    //setEmail method
+    /*setEmail method to set the email of the user
+     *@param email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
-    //getUsername method
+    /*getUserName method to get the username of the user
+     *returns username
+     */
     public String getUsername() {
         return username;
     }
 
-    //setUsername method
+    /*setUsername method to set the username of the user
+     *@param username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
-    //getPhoneNumber method
+    /*getPhoneNumber method to get the phone number of the user
+     *returns phonNumber
+     */
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
-    //setPhoneNumber method
+    /*setPhoneNumber method to set the phone number of the user
+     *@param phoneNumber
+     */
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
-    //getId method
+    /*getId method to get the id of the user
+     *returns id
+     */
     public String getId() {
         return id;
     }
 
-    //setId method
+    /*setId method to set the id of the user
+     *@param id
+     */
     public void setId(String id) {
         this.id = id;
     }
