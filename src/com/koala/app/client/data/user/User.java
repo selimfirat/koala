5bb23193fb410440bc95f6e429f5f1278fd6dc0f
denizevr <deniz.evrensel@ug.bilkent.edu.tr>
2016-12-24 16:32:55 +0300

    Update User.java

--- src/com/koala/app/client/data/user/User.java
+++ src/com/koala/app/client/data/user/User.java
@@ -10,14 +10,15 @@ import org.jongo.marshall.jackson.oid.MongoId;
 public class User {
 
     @MongoId
+    //variables for user object
     private String id;
-
     private String fullName;
     private String password;
     private String email;
     private String username;
     private String phoneNumber;
 
+    //user object with parameters id, fullname, password, email, username, phoneNumber
     public User(String id, String fullName, String password, String email, String username, String phoneNumber) {
         this.id = id;
         this.fullName = fullName;
@@ -27,54 +28,67 @@ public class User {
         this.phoneNumber = phoneNumber;
     }
 
+    //default constructor
     public User() {
 
     }
 
+    //getFullName method
     public String getFullName() {
         return fullName;
     }
 
+    //setFullName method
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
 
+    //getPassword method
     public String getPassword() {
         return password;
     }
 
+    //setPassword method
     public void setPassword(String password) {
         this.password = password;
     }
 
+    //getEmail method
     public String getEmail() {
         return email;
     }
 
+    //setEmail method
     public void setEmail(String email) {
         this.email = email;
     }
 
+    //getUsername method
     public String getUsername() {
         return username;
     }
 
+    //setUsername method
     public void setUsername(String username) {
         this.username = username;
     }
 
+    //getPhoneNumber method
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
+    //setPhoneNumber method
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
+    //getId method
     public String getId() {
         return id;
     }
 
+    //setId method
     public void setId(String id) {
         this.id = id;
     }
