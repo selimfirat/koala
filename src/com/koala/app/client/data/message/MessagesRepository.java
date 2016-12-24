537213e2acf25c0d297f27e28ce4de9e2738a0cc
denizevr <deniz.evrensel@ug.bilkent.edu.tr>
2016-12-24 16:42:22 +0300

    Update MessagesRepository.java

--- src/com/koala/app/client/data/message/MessagesRepository.java
+++ src/com/koala/app/client/data/message/MessagesRepository.java
@@ -11,12 +11,16 @@ import java.util.Date;
  */
 public class MessagesRepository {
 
+    //variables
     private static MessagesRepository _instance;
 
     private MongoCollection messages;
 
     private MessagesRepository() { }
 
+    /*getInstance method to return repository if it exists, otherwise to create one
+     *returns _instance
+     */
     public static MessagesRepository getInstance() {
 
         if (_instance == null)
@@ -25,22 +29,36 @@ public class MessagesRepository {
         return _instance;
     }
 
+    /*setJongo method to set messages in database
+    */
     public void setJongo(Jongo jongo) {
         messages = jongo.getCollection("messages");
     }
 
+    /*findAfter method to find the messages after a specific date
+     *returns null
+     */
     public Observable<Message> findAfter(Date date) {
         return null;
     }
 
+    /*findBefore method to find the messages before a specific date
+     *returns null
+     */
     public Observable<Message> findBefore(Date date) {
         return null;
     }
 
+    /*getMessagesOfCurrentUser method to get messages of a user
+     *returns null
+     */
     public Observable<Message> getMessagesOfCurrentUser() {
         return null;
     }
 
+    /*save method to save messaging history
+     *returns null
+     */
     public Observable<Void> save(Message message) {
         return null;
     }
